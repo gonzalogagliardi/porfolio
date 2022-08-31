9519c699a1641d83673f7e7c25aedf73f67a1013
Gonzalo Gagliardi <76748742+gonzalogagliardi@users.noreply.github.com>
2022-08-30 21:02:45 -0300

    Add files via upload

---
+++ form/form.php
@@ -0,0 +1,24 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['textarea'];
+
+//Como va a llegar el cuerpo
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje = "Su email es " . $email . ",\r\n";
+$mensaje = "Mensaje: " . $_POST['mensaje'] . ",\r\n";
+$mensaje = "Enviado el: " . date('d/m/Y', time ());
+
+//Destinatario
+$destino = 'gagliardigonzalo@gmail.com';
+$asunto = 'Mensaje en porfolio';
+
+
+// Funcion mail
+mail($para , $asunto, utf8_decode($mensaje), $header);
+
+//Redireccion al haber enviado el form
+header('Location:exito.html');
+
+?>
